c2cf09157e1c6dfa215fa02ee8102337de5a07ba
Mohit Bisht <51907464+Mohitb07@users.noreply.github.com>
2020-08-15 17:08:11 +0530

    Updates

---
+++ precache-manifest.505a8525545c55a2abac1cc72f75eea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce6fb1fa144b353fde2605315888caf",
+    "url": "/ReactSeason/index.html"
+  },
+  {
+    "revision": "8b26544e6cacab2731e3",
+    "url": "/ReactSeason/static/css/main.a248eae5.chunk.css"
+  },
+  {
+    "revision": "c2d246ecc2b83ccac6a6",
+    "url": "/ReactSeason/static/js/2.8b77c9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactSeason/static/js/2.8b77c9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b26544e6cacab2731e3",
+    "url": "/ReactSeason/static/js/main.2dc6d885.chunk.js"
+  },
+  {
+    "revision": "9606d67848b758478045",
+    "url": "/ReactSeason/static/js/runtime-main.9d539377.js"
+  }
+]);
